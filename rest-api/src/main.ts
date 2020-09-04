8f8231ffe3ab88bd146e0fa1aab0ba78f0e92104
UtopiaBeam <utopiabeam@gmail.com>
2020-09-04 21:31:19 +0700

    📝 Add swagger

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Assignment I REST endpoints')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
