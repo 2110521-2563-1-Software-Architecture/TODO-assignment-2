4198b03a3e958b1731b675d12de72ad09191d9a8
Purinut Thedwichienchai <purinatth@gmail.com>
2020-09-15 23:34:50 +0700

    add scenario-A-big for GRPC

---
+++ GRPC/scenario-A-big.js
@@ -0,0 +1,28 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function main() {
+    var command = process.argv[2];
+    let exec_command = "";
+    if (command == 'list')
+        exec_command = "node client.js list";
+    else if (command == 'insert')
+        exec_command = "node client.js insert 1 1 1";
+    else if (command == 'get')
+        exec_command = "node client.js get 1";
+    else if (command == 'delete')
+        exec_command = "node client.js delete 1";
+    let dateTime = new Date();
+    const t1 = dateTime.getTime();
+    for(let i = 0;i<100;i++){
+        const {stdout, stderr} = await exec(exec_command);
+        if (i%10 == 0) console.log("stdout: " + `${stdout}`);
+    }
+    let dateTime2 = new Date();
+    const t2 = dateTime2.getTime();
+
+    const responseTime = t2-t1;
+    console.log("Time used: " + responseTime + " milliseconds.");
+}
+
+main()
