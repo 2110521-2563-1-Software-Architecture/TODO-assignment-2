4d9899740727e4d92fdba4802469838501375ed5
Purinut Thedwichienchai <purinatth@gmail.com>
2020-09-16 13:26:50 +0700

    add function repeat to scenario A of GRPC

--- GRPC/scenario-A.js
+++ GRPC/scenario-A.js
@@ -2,13 +2,28 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
+async function repeat(num, exec_command) {
+    let res_list = [];
+
+    for(let i = 1;i<=num;i++){
+        let t1 = new Date().getTime();
+        const {stdout, stderr} = await exec(exec_command);
+        // if (i%10 == 0) console.log("stdout: " + `${stdout}`);
+        let t2 = new Date().getTime();
+        const responseTime = t2-t1;
+        if (i==1) console.log("responseTime: " + responseTime);
+        res_list.push({callNumber: i, responseTime: responseTime});
+    }
+    return res_list;
+}
+
 async function main() {
     var command = process.argv[2];
     var num = process.argv[3];
-    let res_list = [];
+    var scenario = process.argv[4];
 
     const csvWriter = createCsvWriter({
-        path: `../result/Scenario_A_gRPC_${command}.csv`,
+        path: `../result/Scenario_${scenario}_gRPC_${command}.csv`,
         header: [
             { id: "callNumber", title: "Number of call" },
             { id: "responseTime", title: "Response Time" },
@@ -24,22 +39,11 @@ async function main() {
         exec_command = "node client.js get 1";
     else if (command == 'delete')
         exec_command = "node client.js delete 1";
-    
-    for(let i = 0;i<num;i++){
-        let dateTime = new Date();
-        const t1 = dateTime.getTime();
-        const {stdout, stderr} = await exec(exec_command);
-        if (i%10 == 0) console.log("stdout: " + `${stdout}`);
-        let dateTime2 = new Date();
-        const t2 = dateTime2.getTime();
-        const responseTime = t2-t1;
-        res_list.push({callNumber: i+1, responseTime: responseTime});
-    }
-    
+
     // console.log("Time used: " + responseTime + " milliseconds.");
-    
-    const records = res_list;
-    
+
+    const records = await repeat(num, exec_command);
+
     csvWriter.writeRecords(records)       // returns a promise
         .then(() => {
             console.log('...Done');
